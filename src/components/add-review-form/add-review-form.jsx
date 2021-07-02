c580b13f93a07ff5943251379c6cebbba00de779
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-07-02 15:18:13 +0300

    Fixed required fields in Form

--- src/components/add-review-form/add-review-form.jsx
+++ src/components/add-review-form/add-review-form.jsx
@@ -49,6 +49,9 @@ const AddReviewForm = (props) => {
     comment: ``,
   });
 
+  const [isNameMissing, setIsNameMissing] = useState(false);
+  const [isCommentMissing, setIsCommentMissing] = useState(false);
+
   const setName = (evt) => {
     setReview({...review, name: evt.target.value});
     localStorage.setItem(ReviewField.NAME, evt.target.value);
@@ -95,10 +98,34 @@ const AddReviewForm = (props) => {
 
   const hiddenClassName = isVisible ? `reviews__form-wrapper--display-block` : `reviews__form-wrapper--display-none`;
 
+  const hiddenNameClassName = isNameMissing ? `` : `visually-hidden`;
+
+  const hiddenCommentClassName = isCommentMissing ? `` : `visually-hidden`;
+
+  const requiredInputClassName = `add-review-form__input--required`;
+  const nameRequiredClassName = review.name === `` ? requiredInputClassName : ``;
+  const commentRequiredClassName = review.comment === `` ? requiredInputClassName : ``;
+
   const ratingValues = Array.from({length: RATINGS_COUNT}, (_, i) => i + 1);
 
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
+    let hasError = false;
+    if (!review.name || !review.name.trim()) {
+      setIsNameMissing(true);
+      hasError = true;
+    } else {
+      setIsNameMissing(false);
+    }
+    if (!review.comment || !review.comment.trim()) {
+      setIsCommentMissing(true);
+      hasError = true;
+    } else {
+      setIsCommentMissing(false);
+    }
+    if (hasError) {
+      return;
+    }
     onReviewSubmit(review);
     handleClose();
   };
@@ -107,17 +134,12 @@ const AddReviewForm = (props) => {
     <React.Fragment>
       <div className={`reviews__form-wrapper ${hiddenClassName}`}>
         <form ref={ref} className="reviews__form add-review-form" action="#" method="post" onSubmit={handleFormSubmit}>
-          <button className="add-review-form__btn-close" type="button" aria-label="Закрыть попап" onClick={handleClose}>
-            <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.6399 15.0096L7.50482 8.86495L1.36977 15.0096L0 13.6399L6.14469 7.50482L0 1.36978L1.36977 0L7.50482 6.14469L13.6399 0.00964652L15 1.36978L8.86495 7.50482L15 13.6399L13.6399 15.0096Z" fill="#9F9E9E"/>
-            </svg>
-          </button>
           <h2 className="add-review-form__title">Оставить отзыв</h2>
-          <p className="add-review-form__text">Пожалуйста, заполните поле</p>
           <div className="add-review-form__fields-wrapper">
             <div className="add-review-form__section add-review-form__section--first">
-              <div className="add-review-form__field">
-                <input className="add-review-form__input add-review-form__input--required" type="text" placeholder="Имя" name="user-name" id="user-name"required autoFocus={true} ref={inputEl} onChange={setName} value={review.name}/>
+              <div className="add-review-form__field add-review-form__field--required">
+                <span className={`add-review-form__text ${hiddenNameClassName}`}>Пожалуйста, заполните поле</span>
+                <input className={`add-review-form__input ${nameRequiredClassName}`} type="text" placeholder="Имя" name="user-name" id="user-name" autoFocus={true} ref={inputEl} onChange={setName} value={review.name}/>
                 <label className="add-review-form__label visually-hidden" htmlFor="user-name" >Имя</label>
               </div>
               <div className="add-review-form__field">
@@ -143,12 +165,18 @@ const AddReviewForm = (props) => {
                 }
               </div>
               <div className="add-review-form__comment">
-                <textarea className="add-review-form__textarea add-review-form__input--required" name="review-text" id="review-text" placeholder="Комментарий" required onChange={setComment} value={review.comment}/>
+                <span className={`add-review-form__text ${hiddenCommentClassName}`}>Пожалуйста, заполните поле</span>
+                <textarea className={`add-review-form__textarea ${commentRequiredClassName}`} name="review-text" id="review-text" placeholder="Комментарий" onChange={setComment} value={review.comment}/>
               </div>
             </div>
           </div>
 
           <button className="add-review-form__btn-submit" type="submit">Оставить отзыв</button>
+          <button className="add-review-form__btn-close" type="button" aria-label="Закрыть попап" onClick={handleClose}>
+            <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13.6399 15.0096L7.50482 8.86495L1.36977 15.0096L0 13.6399L6.14469 7.50482L0 1.36978L1.36977 0L7.50482 6.14469L13.6399 0.00964652L15 1.36978L8.86495 7.50482L15 13.6399L13.6399 15.0096Z" fill="#9F9E9E"/>
+            </svg>
+          </button>
         </form>
       </div>
     </React.Fragment>
